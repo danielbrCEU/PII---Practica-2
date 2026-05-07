517be378853ba2d68f3ca025bfa0a97fc44c54e1
Daniel Mossberg Bello <d.mossberg.bello@gmail.com>
2026-05-07 20:05:08 +0200

    Operadores de comparacion

--- src/Punto.h
+++ src/Punto.h
@@ -3,4 +3,16 @@
 struct Punto {
     int x;
     int y;
+
+    bool operator==(const Punto& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator<(const Punto& other) const {
+        return x < other.x || (x == other.x && y < other.y);
+    }
+
+    bool operator>(const Punto& other) const {
+        return other < *this; // reutiliza el 
+    }
 };
